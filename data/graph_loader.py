8c4fc0e871f7bac9a11defae1436533866e0f6f8
pereja12 <jaimeignacio.perez@forvia.com>
2025-11-29 13:18:22 -0600

    Refactor the graph loader to have both edge types (walk and stops) in diferent functions within the create_graph_transit method, bring implementation from the notebook

--- data/graph_loader.py
+++ data/graph_loader.py
@@ -8,7 +8,7 @@ import pickle
 
 class GraphLoader:
 
-    def create_graph_walk(self, graphfile_path, geojson_poly_path):
+    def create_graph_walk(self, graphfile_path, geojson_poly_path) -> nx.MultiGraph:
 
         graphml_path = f"{graphfile_path}.graphml"
         graphpkl_path = f"{graphfile_path}.pkl"
@@ -47,19 +47,51 @@ class GraphLoader:
 
     def create_graph_transit(self, stops_df: pd.DataFrame) -> nx.MultiDiGraph:
         # create graph transit adjacency list in stops_df
-        G = nx.MultiDiGraph()
+        graph_transit = nx.MultiDiGraph()
         
         # To have the geometry (Points) in meters for distance calculations
         stops_gdf = gpd.GeoDataFrame(stops_df, geometry='geometry', crs="EPSG:4326").to_crs(epsg=3857)
 
-        for idx, stop in stops_gdf.iterrows():
-            stop_id = stop['stop_id']
-            G.add_node(stop_id, pos=stop['geometry'], stop_name=stop['stop_name'], routes=stop['routes_by_stop'], shapes=stop['shapes_by_stop'])
-            next_stops = stop['next_stop_id']
-            for next_stop_id, edge_list in next_stops.items():
-                for edge in edge_list:                
-                    G.add_edge(stop_id, next_stop_id, weight=edge['weight'], shape_id=edge['shape_id'], frequency=edge['frequency'])
-        return G
+        add_adjacent_stops(graph_transit, stops_gdf)
+        add_walking_edges(graph_transit, stops_gdf)
 
-    
+        return graph_transit
 
+
+def add_adjacent_stops(graph_transit: nx.MultiDiGraph, stops_gdf:gpd.GeoDataFrame):    
+
+    for idx, stop in stops_gdf.iterrows():
+        stop_id = stop['stop_id']
+        graph_transit.add_node(stop_id, pos=stop['geometry'], stop_name=stop['stop_name'], routes=stop['routes_by_stop'], shapes=stop['shapes_by_stop'])
+        next_stops = stop['next_stop_id']
+        for next_stop_id, edge_list in next_stops.items():
+            for edge in edge_list:                
+                graph_transit.add_edge(stop_id, next_stop_id, weight=edge['weight'], shape_id=edge['shape_id'], frequency=edge['frequency'])
+
+def add_walking_edges(graph_transit: nx.MultiDiGraph, stops_gdf: gpd.GeoDataFrame, max_walking_time: int = 300):
+
+    walking_speed_mps = 5 / 3.6 # average walking speed 5 km/h in m/s
+    walking_distance = max_walking_time * walking_speed_mps  
+
+    # Use spatial index to efficiently find nearby stops
+    sindex = stops_gdf.sindex
+
+    for stop_idx, stop in stops_gdf.iterrows():
+        stop_id = stop['stop_id']
+        stop_point_geo = stop['geometry']
+        stop_circle_walking = stop_point_geo.buffer(walking_distance)
+
+        # candidate indices whose geometries intersect the walking reach
+        nearby_stops_idx = sindex.query(stop_circle_walking, predicate="intersects")
+
+        for nearby_stop_idx in nearby_stops_idx:
+            # we dont want to add walking edge to itself
+            if nearby_stop_idx == stop_idx:
+                continue
+            nearby_stop = stops_gdf.iloc[nearby_stop_idx]
+            #calculate real distance in meters between the two stops
+            distance = stop_point_geo.distance(nearby_stop['geometry'])            
+            # walking time in seconds
+            walking_time = round(distance / walking_speed_mps) 
+            # add walking edge, shape_id='walking' due to the mode of transport, frequency=0 as its not a scheduled transport you have to wait
+            graph_transit.add_edge(stop_id, nearby_stop['stop_id'], weight=walking_time, shape_id='walking', frequency=0)
